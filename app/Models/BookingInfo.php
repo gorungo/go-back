338f34f1b92d5a98ff599887e8f4305a1e1157d2
inkas <denis.g.petrov@gmail.com>
2021-05-17 16:49:33 +1000

    add whatsapp to booking

--- app/Models/BookingInfo.php
+++ app/Models/BookingInfo.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class BookingInfo extends Model
 {
     protected $table = 'booking_infos';
-    protected $fillable = ['info', 'contacts'];
+    protected $fillable = ['info', 'contacts', 'whatsapp'];
 
     public $timestamps = false;
 }
