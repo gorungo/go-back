cda6ae011687aedb1c2968a126573c99990e7270
inkas <denis.g.petrov@gmail.com>
2021-05-17 16:51:56 +1000

    add whatsapp to booking

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -28,7 +28,7 @@ class User extends JsonResource
                 'email' => $this->email,
                 'profile_hid' => $this->profile->hid,
                 'display_name' => $this->displayName,
-                'booking_whatsapp' => $this->bookingInfo ? $this->bookingInfo->whatsapp : '',
+                'booking_whatsapp' => $this->profile->bookingInfo ? $this->profile->bookingInfo->whatsapp : '',
                 'image_url' => $this->imageUrl,
                 'superuser' => Auth::user() ? $this->when(Auth::user()->hasAnyRole(['admin', 'super-admin']), true):null,
             ],
