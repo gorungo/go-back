a23d87759b54c0e2515cdb4fd9cf5624bbcfe01b
inkas <denis.g.petrov@gmail.com>
2021-07-02 12:16:06 +1000

    add dates order

--- app/Models/Idea.php
+++ app/Models/Idea.php
@@ -848,7 +848,7 @@ class Idea extends Model
 
     public function scopeJoinIdeaDates($query)
     {
-        return $query->join('idea_dates', function ($join) {
+        return $query->leftJoin('idea_dates', function ($join) {
         $join
             ->on('ideas.id', '=', 'idea_dates.idea_id')
             ->whereRaw("TO_DAYS(NOW()) <= TO_DAYS(`start_date`)");
