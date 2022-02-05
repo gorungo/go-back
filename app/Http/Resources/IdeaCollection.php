f867647f7f1f60dc512d38d78d0a9abafcefaaed
inkas <denis.g.petrov@gmail.com>
2022-02-05 12:18:43 +1000

    update idea resource

--- app/Http/Resources/IdeaCollection.php
+++ app/Http/Resources/IdeaCollection.php
@@ -16,13 +16,13 @@ class IdeaCollection extends ResourceCollection
     {
         return [
             'data' => Idea::collection($this->collection),
-            'pagination' => [
-            'total' => $this->total(),
-            'count' => $this->count(),
-            'per_page' => $this->perPage(),
-            'current_page' => $this->currentPage(),
-            'total_pages' => $this->lastPage()
-        ],
         ];
+    }
+
+    public function withResponse($request, $response)
+    {
+        $jsonResponse = json_decode($response->getContent(), true);
+        unset($jsonResponse['links'],$jsonResponse['meta']);
+        $response->setContent(json_encode($jsonResponse));
     }
 }
